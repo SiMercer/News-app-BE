2068aa60d8b4b079a6a4ea2aa90690184233574f
SiMercer <na>
2025-06-26 21:26:04 +0100

    api location updated

--- app.js
+++ app.js
@@ -24,32 +24,28 @@ const {
 } = require("./controllers/users.controllers");
 
 app.use(cors());
-
 app.use(express.json());
 
-app.get("/api", apiRequest);
+// Use a base path for the API
+const apiBase = "/News-app-BE-api";
 
-app.get("/api/topics", getTopics);
+app.get(`${apiBase}`, apiRequest);
 
-app.get("/api/articles", getArticles);
+app.get(`${apiBase}/topics`, getTopics);
 
-app.post("/api/articles", postArticle);
+app.get(`${apiBase}/articles`, getArticles);
+app.post(`${apiBase}/articles`, postArticle);
+app.get(`${apiBase}/articles/:article_id`, getArticleByID);
+app.patch(`${apiBase}/articles/:article_id`, patchArticleByID);
 
-app.get("/api/articles/:article_id", getArticleByID);
+app.get(`${apiBase}/articles/:article_id/comments`, getCommentsByArticleByID);
+app.post(`${apiBase}/articles/:article_id/comments`, postCommentsByArticleByID);
 
-app.patch("/api/articles/:article_id", patchArticleByID);
+app.patch(`${apiBase}/comments/:comment_id`, patchCommentVotes);
+app.delete(`${apiBase}/comments/:comment_id`, deleteCommentByID);
 
-app.get("/api/articles/:article_id/comments", getCommentsByArticleByID);
-
-app.post("/api/articles/:article_id/comments", postCommentsByArticleByID);
-
-app.patch("/api/comments/:comment_id", patchCommentVotes);
-
-app.delete("/api/comments/:comment_id", deleteCommentByID);
-
-app.get("/api/users", getUsers);
-
-app.get("/api/users/:username", getUserByUsername);
+app.get(`${apiBase}/users`, getUsers);
+app.get(`${apiBase}/users/:username`, getUserByUsername);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "path not found" });
@@ -58,11 +54,10 @@ app.all("*", (req, res) => {
 app.use((err, req, res, next) => {
   if (err.code === "23503") {
     res.status(404).send({ msg: "Not found" });
-  }
-  if (err.code === "22P02") {
+  } else if (err.code === "22P02") {
     res.status(400).send({ msg: "Bad request" });
   } else {
-    res.status(err.status).send({ msg: err.msg });
+    res.status(err.status || 500).send({ msg: err.msg || "Internal server error" });
   }
 });
 
